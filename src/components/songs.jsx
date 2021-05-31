5e5ee77ab279e58e0a9bb8baabab242f61e3de34
Tomas <tomas.tomas44@gmail.com>
2021-05-31 17:40:04 +0300

    added song count in database on top

--- src/components/songs.jsx
+++ src/components/songs.jsx
@@ -12,38 +12,45 @@ class Songs extends Component {
   };
 
   render() {
+    const { length: songCount } = this.state.songs;
+    if (songCount === 0) return <h2>There are no songs in the database</h2>;
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Song Title</th>
-            <th>Artist</th>
-            <th>Album</th>
-            <th>Genre</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.songs.map((song) => (
-            <tr key={song._id}>
-              <td>{song.title}</td>
-              <td>{song.artist}</td>
-              <td>{song.album}</td>
-              <td>{song.genre}</td>
-              <td>
-                <button
-                  onClick={() => {
-                    this.handleDelete(song);
-                  }}
-                  className="btn btn-danger btn-sm"
-                >
-                  Delete
-                </button>
-              </td>
+      <React.Fragment>
+        <p className="mb-1 pb-1 pt-1 mx-auto">
+          There are {songCount} songs in the database.
+        </p>
+        <table className="table table-dark">
+          <thead>
+            <tr>
+              <th>Song Title</th>
+              <th>Artist</th>
+              <th>Album</th>
+              <th>Genre</th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {this.state.songs.map((song) => (
+              <tr key={song._id}>
+                <td>{song.title}</td>
+                <td>{song.artist}</td>
+                <td>{song.album}</td>
+                <td>{song.genre}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      this.handleDelete(song);
+                    }}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
     );
   }
 }
