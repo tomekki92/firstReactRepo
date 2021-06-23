066e9282e13a456269af213dba9fd8a01b5bb3db
Tomas <tomas.tomas44@gmail.com>
2021-06-23 13:08:15 +0300

    fix pagination anchor bug

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -14,9 +14,9 @@ const Pagination = (props) => {
             key={p}
             className={currentPage === p ? "page-item active" : "page-item"}
           >
-            <a onClick={() => onPageChange(p)} className="page-link">
+            <button onClick={() => onPageChange(p)} className="page-link">
               {p}
-            </a>
+            </button>
           </li>
         ))}
       </ul>
